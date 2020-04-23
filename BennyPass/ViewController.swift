4671784b1ee1143cea3b8617ce4caa820d244f16
Andrii Zenko <camel.ua@gmail.com>
2020-04-23 15:28:40 +0300

    Initial Commit

---
+++ BennyPass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BennyPass
+//
+//  Created by Camel on 23.04.2020.
+//  Copyright © 2020 Camel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
