ed4716d43ea8effc69f3b406536f3f9950a2b9bd
Andrii Zenko <camel.ua@gmail.com>
2020-04-29 16:28:53 +0300

    Add views on Storyboard

---
+++ BennyPass/AuthorizationController.swift
@@ -0,0 +1,25 @@
+//
+//  AuthorizationController.swift
+//  BennyPass
+//
+//  Created by Camel on 29.04.2020.
+//  Copyright © 2020 Camel. All rights reserved.
+//
+
+import UIKit
+
+class AuthorizationController: UIViewController {
+
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func confirmButton(_ sender: UIButton) {
+        
+    }
+    
+}
